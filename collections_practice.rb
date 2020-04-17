939c2986eb6d478d2be1b666870609ae80c9d2cb
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 01:42:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -32,5 +32,5 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each {|string| string.push("s") unless string == array[2]}
+  array.each {|string| string + "s" unless string == array[2]}
 end
