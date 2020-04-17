b4a510c8f90fe6bfa5d774af28ca0c3683a464d1
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 00:53:20 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def sort_array_asc(array)
   array.sort
 end
 
-def sort_array_dec(array)
+def sort_array_desc(array)
   reverse_array = []
   reverse_array << array.collect {|i| -i.length}
   reverse_array.sort
