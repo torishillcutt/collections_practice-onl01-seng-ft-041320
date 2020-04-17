6ed5a23a58765dc73d3550da32bb4d55d727a6da
Victoria Shillcutt <torishillcutt@gmail.com>
2020-04-17 01:09:05 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -11,8 +11,6 @@ def sort_array_char_count(array)
 end
 
 def swap_elements(array)
-  array.sort do |a, b|
-    array[1] <=> array[2]
-  end
-  array
+array[1], array[2] = array[2], array[1]
+array
 end
